4fffcef1114a41d1324dac77f031b48d8c2810ea
AOKI <aoki@limbate.com>
2015-02-01 22:38:53 +0900

    Add Dynamic Framework Target

---
+++ Finch iOS/Finch-iOS.h
@@ -0,0 +1,27 @@
+//
+//  Finch iOS.h
+//  Finch iOS
+//
+//  Created by AOKI Yuuto on 2015/02/01.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Finch iOS.
+FOUNDATION_EXPORT double Finch_iOSVersionNumber;
+
+//! Project version string for Finch iOS.
+FOUNDATION_EXPORT const unsigned char Finch_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Finch_iOS/PublicHeader.h>
+
+#import "FIError.h"
+#import "FISampleBuffer.h"
+#import "FISampleDecoder.h"
+#import "FISampleFormat.h"
+#import "FISound.h"
+#import "FISoundContext.h"
+#import "FISoundDevice.h"
+#import "FISoundEngine.h"
+#import "FISoundSource.h"
---
+++ Finch iOSTests/Finch_iOSTests.m
@@ -0,0 +1,40 @@
+//
+//  Finch_iOSTests.m
+//  Finch iOSTests
+//
+//  Created by AOKI Yuuto on 2015/02/01.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Finch_iOSTests : XCTestCase
+
+@end
+
+@implementation Finch_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
